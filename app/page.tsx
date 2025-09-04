6ae127d6e2a1e9147974185effcaa1eb1448ab29
Tarasato <taramiratsu@gmail.com>
2025-09-04 13:38:09 +0700

    changed name

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="">
-      <h1>Food Tracker by Tarasato</h1>
+      <h1>Food Tracker by Kanchai AKA Tarasato</h1>
       <h3>ยินดีต้อนรับ</h3>
     </div>
   );
